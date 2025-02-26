261f1c685e89c5a70c1263435d66c1c816f83f79
Yassine <yassineparis26@gmain.com>
2025-02-26 09:35:22 +0000

    some new files

---
+++ Makefile
@@ -0,0 +1,4 @@
+NAME = so_long
+src = src/main.c src/free.c src/reading_map.c src/extention_checker.c src/error.c src/checking_map.c libft/ft_itoa.c \
+		libft/ft_split.c libft/ft_strchr.c libft/strdup.c libft/ft_strjoin.c libft/ft_strlcat.c libft/strlcpy.c \
+		libft/ft_strlen.c libft/strnstr.c libft/ft_substr.c
--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: ykabili- <ykabili-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/23 15:57:47 by yassine           #+#    #+#             */
-/*   Updated: 2025/02/23 18:00:15 by ykabili-         ###   ########.fr       */
+/*   Updated: 2025/02/24 16:51:26 by ykabili-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,37 @@
 # define SO_LONG_H
 
 # include <stdlib.h>
+# include <string.h>
+# include <stddef.h>
+# include <ctype.h>
+# include <mlx.h>
 # include <stdio.h>
 # include <unistd.h>
 # include <fcntl.h>
+# include "mlx.h"
 # include "libft/libft.h"
+
+typedef struct a_data
+{
+	char	**map_ptr;
+	int	x_cor;
+	int	y_cor;
+	int	coins;
+	int	exit;
+	int	nb_lines;
+	int	line_len;
+}
 
 int		main(int ac, char **av);
 void	ft_error(char *str);
+void	ft_ex_checker(char *str);
+char	**reading_map(int fd);
+void	checking_map(char **map);
+void	ft_free(char **s);
+int		path_checker(char **map, int n);
+void	ft_save_p_cor(t_data *data, char **map);
+void	ft_alloc_map(t_data *data, char **map);
+void	checking_path(t_data, data->x_cor, data->y_cor, n);
+void	free_data(t_data *data);
 
 #endif
